f8b4801b42345e3261368070b7c032af4b5804c3
ayanaser12345 <ayanaser933@gmail.com>
2025-03-16 17:14:36 +0200

    Update server1.js

--- server1.js
+++ server1.js
@@ -3,16 +3,13 @@ const cors = require("cors");
 
 const app = express();
 
-// Middleware
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json()); // Parse JSON request bodies
 
-// Test route
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to our API!" });
 });
 
-// API route to handle user data
 app.post("/users", (req, res) => {
     const { name, email } = req.body;
     if (!name || !email) {
